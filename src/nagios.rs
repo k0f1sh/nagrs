c8d5bee17deb27b63265e8b2696605006bf3593e
k0f1sh <2574472+k0f1sh@users.noreply.github.com>
2022-03-22 22:51:51 +0900

    refactoring

--- src/nagios.rs
+++ src/nagios.rs
@@ -4,25 +4,6 @@ use std::io::{self, BufRead, Read};
 use std::path::Path;
 use std::{error, fmt};
 
-type NagiosInfo = HashMap<String, String>;
-type NagiosProgram = HashMap<String, String>;
-
-type NagiosHost = HashMap<String, String>;
-type NagiosHostName = String;
-
-type NagiosService = HashMap<String, String>;
-
-type NagiosContact = HashMap<String, String>;
-
-#[derive(Debug)]
-pub struct NagiosStatus {
-    pub info: NagiosInfo,
-    pub program: NagiosProgram,
-    pub hosts: HashMap<NagiosHostName, NagiosHost>,
-    pub services: HashMap<NagiosHostName, Vec<NagiosService>>,
-    pub contacts: Vec<NagiosContact>,
-}
-
 #[derive(Debug, PartialEq)]
 enum BlockType {
     Info,
@@ -30,11 +11,32 @@ enum BlockType {
     Host,
     Service,
     Contact,
+    Unkown,
+}
+
+#[derive(Debug, PartialEq)]
+struct Block {
+    block_type: BlockType,
+    key_values: HashMap<String, String>,
+}
+
+impl Block {
+    fn new() -> Self {
+        Block {
+            block_type: BlockType::Unkown,
+            key_values: HashMap::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct NagiosStatus {
+    blocks: Vec<Block>,
 }
 
 #[derive(Debug, PartialEq)]
 enum ParseState {
-    WithinBlock(BlockType),
+    WithinBlock,
     Outside,
 }
 
@@ -54,107 +56,53 @@ impl NagiosStatus {
             .filter(|line| line.len() > 0)
             .filter(|line| line.chars().nth(0).unwrap() != '#');
 
-        let mut info: NagiosInfo = HashMap::new();
-        let mut program: NagiosProgram = HashMap::new();
-        let mut hosts: HashMap<NagiosHostName, NagiosHost> = HashMap::new();
-        let mut services: HashMap<NagiosHostName, Vec<NagiosService>> = HashMap::new();
-        let mut contacts: Vec<NagiosContact> = vec![];
-
-        let mut tmp_host: NagiosHost = HashMap::new();
-        let mut tmp_service: NagiosService = HashMap::new();
-        let mut tmp_contact: NagiosContact = HashMap::new();
+        let mut status = NagiosStatus { blocks: Vec::new() };
+        let mut tmp_block = Block::new();
 
         let mut current_state: ParseState = ParseState::Outside;
 
         for line in lines {
             match &current_state {
-                ParseState::Outside => {
-                    match line.as_str() {
-                        // start block
-                        "info {" => current_state = ParseState::WithinBlock(BlockType::Info),
-                        "programstatus {" => {
-                            current_state = ParseState::WithinBlock(BlockType::Program)
-                        }
-                        "hoststatus {" => current_state = ParseState::WithinBlock(BlockType::Host),
-                        "servicestatus {" => {
-                            current_state = ParseState::WithinBlock(BlockType::Service)
-                        }
-                        "contactstatus {" => {
-                            current_state = ParseState::WithinBlock(BlockType::Contact)
-                        }
-                        _ => {
-                            return Err(ParseError::UnexpectedBlockName(line.clone()));
-                        }
+                ParseState::Outside => match Self::select_block_type(line.as_str()) {
+                    Ok(block_type) => {
+                        tmp_block.block_type = block_type;
+                        current_state = ParseState::WithinBlock;
                     }
-                }
-                ParseState::WithinBlock(block_type) => {
-                    match (block_type, line.as_str()) {
-                        // within block
-                        (BlockType::Host, "}") => match tmp_host.get("host_name") {
-                            Some(key) => {
-                                let key = key.clone();
-                                hosts.insert(key, tmp_host);
-                                tmp_host = HashMap::new();
-                                current_state = ParseState::Outside;
-                            }
-                            None => {
-                                return Err(ParseError::HostNameDoesNotExist);
-                            }
-                        },
-                        (BlockType::Service, "}") => {
-                            let key = tmp_service.get("host_name").unwrap().clone();
-                            let v = services.get_mut(&key);
-                            match v {
-                                None => {
-                                    services.insert(key, vec![tmp_service]);
-                                }
-                                Some(v) => v.push(tmp_service),
-                            }
-                            tmp_service = HashMap::new();
-                            current_state = ParseState::Outside;
-                        }
-                        (BlockType::Contact, "}") => {
-                            contacts.push(tmp_contact);
-                            tmp_contact = HashMap::new();
-                            current_state = ParseState::Outside;
-                        }
-                        (_, "}") => {
-                            current_state = ParseState::Outside;
-                        }
-                        (block_type, s) => match s.split_once('=') {
-                            Some((key, value)) => match block_type {
-                                BlockType::Info => {
-                                    info.insert(key.to_string(), value.to_string());
-                                }
-                                BlockType::Program => {
-                                    program.insert(key.to_string(), value.to_string());
-                                }
-                                BlockType::Host => {
-                                    tmp_host.insert(key.to_string(), value.to_string());
-                                }
-                                BlockType::Service => {
-                                    tmp_service.insert(key.to_string(), value.to_string());
-                                }
-                                BlockType::Contact => {
-                                    tmp_contact.insert(key.to_string(), value.to_string());
-                                }
-                            },
-                            None => {
-                                return Err(ParseError::InvalidKeyValue(s.to_string()));
-                            }
-                        },
+                    Err(error) => return Err(error),
+                },
+                ParseState::WithinBlock => match line.as_str() {
+                    "}" => {
+                        // end block
+                        status.blocks.push(tmp_block);
+                        tmp_block = Block::new();
+                        current_state = ParseState::Outside;
                     }
-                }
+                    s => match s.split_once('=') {
+                        Some((key, value)) => {
+                            tmp_block
+                                .key_values
+                                .insert(key.to_string(), value.to_string());
+                        }
+                        None => {
+                            return Err(ParseError::InvalidKeyValue(s.to_string()));
+                        }
+                    },
+                },
             }
         }
 
-        Ok(NagiosStatus {
-            info,
-            program,
-            hosts,
-            services,
-            contacts,
-        })
+        Ok(status)
+    }
+
+    fn select_block_type(line: &str) -> Result<BlockType> {
+        match line {
+            "info {" => Ok(BlockType::Info),
+            "programstatus {" => Ok(BlockType::Program),
+            "hoststatus {" => Ok(BlockType::Host),
+            "servicestatus {" => Ok(BlockType::Service),
+            "contactstatus {" => Ok(BlockType::Contact),
+            _ => Err(ParseError::UnexpectedLine(line.to_string())),
+        }
     }
 }
 
@@ -163,8 +111,7 @@ type Result<T> = std::result::Result<T, ParseError>;
 #[derive(Debug)]
 pub enum ParseError {
     IoError(io::Error),
-    UnexpectedBlockName(String),
-    HostNameDoesNotExist,
+    UnexpectedLine(String),
     InvalidKeyValue(String),
 }
 
@@ -172,8 +119,7 @@ impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::IoError(err) => write!(f, "{}", err),
-            Self::UnexpectedBlockName(s) => write!(f, "unexpected block name: {}", s),
-            Self::HostNameDoesNotExist => write!(f, "host_name does not exist"),
+            Self::UnexpectedLine(s) => write!(f, "unexpected line: {}", s),
             Self::InvalidKeyValue(s) => write!(f, "invalid line: {}", s),
         }
     }
@@ -183,8 +129,7 @@ impl error::Error for ParseError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
             Self::IoError(err) => Some(err),
-            Self::UnexpectedBlockName(_) => None,
-            Self::HostNameDoesNotExist => None,
+            Self::UnexpectedLine(_) => None,
             Self::InvalidKeyValue(_) => None,
         }
     }
@@ -218,21 +163,9 @@ mod tests {
                 state_type=1
             }
 
-            hoststatus {
-                host_name=web02
-                state_type=1
-                hoge=
-            }
-
             servicestatus {
                 host_name=web01
                 service_description=PING
-            }
-
-            servicestatus {
-                host_name=web01
-                service_description=PONG
-                a=b=c
             }
 
             contactstatus {
@@ -250,58 +183,45 @@ mod tests {
             ("created".to_string(), "123456789".to_string()),
             ("version".to_string(), "9.99".to_string()),
         ]);
-        assert_eq!(nagios_status.info, expected);
+        let block = nagios_status.blocks.get(0).unwrap();
+        assert_eq!(block.block_type, BlockType::Info);
+        assert_eq!(block.key_values, expected);
 
         // program
         let expected = HashMap::from([
             ("daemon_mode".to_string(), "1".to_string()),
             ("nagios_pid".to_string(), "99999".to_string()),
         ]);
-        assert_eq!(nagios_status.program, expected);
+        let block = nagios_status.blocks.get(1).unwrap();
+        assert_eq!(block.block_type, BlockType::Program);
+        assert_eq!(block.key_values, expected);
 
-        // hosts
+        // host
         let expected = HashMap::from([
-            (
-                "web01".to_string(),
-                HashMap::from([
-                    ("host_name".to_string(), "web01".to_string()),
-                    ("state_type".to_string(), "1".to_string()),
-                ]),
-            ),
-            (
-                "web02".to_string(),
-                HashMap::from([
-                    ("host_name".to_string(), "web02".to_string()),
-                    ("state_type".to_string(), "1".to_string()),
-                    ("hoge".to_string(), "".to_string()),
-                ]),
-            ),
+            ("host_name".to_string(), "web01".to_string()),
+            ("state_type".to_string(), "1".to_string()),
         ]);
-        assert_eq!(nagios_status.hosts, expected);
+        let block = nagios_status.blocks.get(2).unwrap();
+        assert_eq!(block.block_type, BlockType::Host);
+        assert_eq!(block.key_values, expected);
 
         // services
-        let expected = HashMap::from([(
-            "web01".to_string(),
-            vec![
-                HashMap::from([
-                    ("host_name".to_string(), "web01".to_string()),
-                    ("service_description".to_string(), "PING".to_string()),
-                ]),
-                HashMap::from([
-                    ("host_name".to_string(), "web01".to_string()),
-                    ("service_description".to_string(), "PONG".to_string()),
-                    ("a".to_string(), "b=c".to_string()),
-                ]),
-            ],
-        )]);
-        assert_eq!(nagios_status.services, expected);
+        let expected = HashMap::from([
+            ("host_name".to_string(), "web01".to_string()),
+            ("service_description".to_string(), "PING".to_string()),
+        ]);
+        let block = nagios_status.blocks.get(3).unwrap();
+        assert_eq!(block.block_type, BlockType::Service);
+        assert_eq!(block.key_values, expected);
 
         // contacts
-        let expected = vec![HashMap::from([
+        let expected = HashMap::from([
             ("contact_name".to_string(), "nagiosadmin".to_string()),
             ("modified_attributes".to_string(), "0".to_string()),
-        ])];
-        assert_eq!(nagios_status.contacts, expected);
+        ]);
+        let block = nagios_status.blocks.get(4).unwrap();
+        assert_eq!(block.block_type, BlockType::Contact);
+        assert_eq!(block.key_values, expected);
     }
 
     #[test]
@@ -317,27 +237,8 @@ mod tests {
         let result = NagiosStatus::parse(buf);
 
         match result {
-            Err(ParseError::UnexpectedBlockName(s)) => {
+            Err(ParseError::UnexpectedLine(s)) => {
                 assert_eq!(s, "piyo {".to_string())
-            }
-            _ => assert!(false),
-        }
-    }
-
-    #[test]
-    fn parse_error_host_name_does_not_exist() {
-        let status_dat = r#"
-            hoststatus {
-                hoge=fuga
-            }
-        "#;
-
-        let buf = io::BufReader::new(status_dat.as_bytes());
-        let result = NagiosStatus::parse(buf);
-
-        match result {
-            Err(ParseError::HostNameDoesNotExist) => {
-                assert!(true)
             }
             _ => assert!(false),
         }
