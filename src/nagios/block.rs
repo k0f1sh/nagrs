5d209f4d47d144dfb7a8e16aa3d68c498e27ed1a
k0f1sh <2574472+k0f1sh@users.noreply.github.com>
2022-04-23 20:22:21 +0900

    move test code

--- src/nagios/block.rs
+++ src/nagios/block.rs
@@ -99,3 +99,112 @@ impl Block {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const STATUS_DAT: &str = r#"
+    info {
+        created=123456789
+        version=9.99
+    }
+
+    programstatus {
+        daemon_mode=1
+        nagios_pid=99999
+    }
+
+    hoststatus {
+        host_name=web01
+        notifications_enabled=1
+        active_checks_enabled=1
+        passive_checks_enabled=1
+        obsess=1
+        event_handler_enabled=1
+        flap_detection_enabled=1
+    }
+
+    servicestatus {
+        host_name=web01
+        service_description=PING
+        notifications_enabled=1
+        active_checks_enabled=1
+        passive_checks_enabled=1
+        check_command=hoge
+        obsess=1
+        event_handler_enabled=1
+        flap_detection_enabled=1
+
+    }
+
+    contactstatus {
+        contact_name=nagiosadmin
+        modified_attributes=0
+        }
+    
+    "#;
+
+    #[test]
+    fn parse() {
+        let buf = io::BufReader::new(STATUS_DAT.as_bytes());
+        let blocks = Block::parse(buf).unwrap();
+
+        // info
+        let expected = HashMap::from([
+            ("created".to_string(), "123456789".to_string()),
+            ("version".to_string(), "9.99".to_string()),
+        ]);
+        let block = blocks.get(0).unwrap();
+        assert_eq!(block.block_type, BlockType::Info);
+        assert_eq!(block.key_values, expected);
+
+        // program
+        let expected = HashMap::from([
+            ("daemon_mode".to_string(), "1".to_string()),
+            ("nagios_pid".to_string(), "99999".to_string()),
+        ]);
+        let block = blocks.get(1).unwrap();
+        assert_eq!(block.block_type, BlockType::Program);
+        assert_eq!(block.key_values, expected);
+
+        // host
+        let expected = HashMap::from([
+            ("host_name".to_string(), "web01".to_string()),
+            ("notifications_enabled".to_string(), "1".to_string()),
+            ("active_checks_enabled".to_string(), "1".to_string()),
+            ("passive_checks_enabled".to_string(), "1".to_string()),
+            ("obsess".to_string(), "1".to_string()),
+            ("event_handler_enabled".to_string(), "1".to_string()),
+            ("flap_detection_enabled".to_string(), "1".to_string()),
+        ]);
+        let block = blocks.get(2).unwrap();
+        assert_eq!(block.block_type, BlockType::Host);
+        assert_eq!(block.key_values, expected);
+
+        // services
+        let expected = HashMap::from([
+            ("host_name".to_string(), "web01".to_string()),
+            ("service_description".to_string(), "PING".to_string()),
+            ("notifications_enabled".to_string(), "1".to_string()),
+            ("active_checks_enabled".to_string(), "1".to_string()),
+            ("passive_checks_enabled".to_string(), "1".to_string()),
+            ("check_command".to_string(), "hoge".to_string()),
+            ("obsess".to_string(), "1".to_string()),
+            ("event_handler_enabled".to_string(), "1".to_string()),
+            ("flap_detection_enabled".to_string(), "1".to_string()),
+        ]);
+        let block = blocks.get(3).unwrap();
+        assert_eq!(block.block_type, BlockType::Service);
+        assert_eq!(block.key_values, expected);
+
+        // contacts
+        let expected = HashMap::from([
+            ("contact_name".to_string(), "nagiosadmin".to_string()),
+            ("modified_attributes".to_string(), "0".to_string()),
+        ]);
+        let block = blocks.get(4).unwrap();
+        assert_eq!(block.block_type, BlockType::Contact);
+        assert_eq!(block.key_values, expected);
+    }
+}
